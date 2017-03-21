241b54823e422785fa7b20075056c3f338ec81c9
Robert Sherling <rob@robsherling.com>
2017-03-21 12:23:36 -0400

    Add list items and animations

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import {
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  LayoutAnimation
+} from 'react-native';
 import { CardSection } from './common';
+import { connect } from 'react-redux';
+// Import everything from a file
+import * as actions from '../actions';
 
 class ListItem extends Component {
+
+  componentWillUpdate(){
+    LayoutAnimation.spring();
+  }
+  
+  renderDescription() {
+    const { library, expanded } = this.props;
+    if (expanded) {
+      return (
+        <CardSection>
+          <Text style={{paddingLeft: 10}}>{library.description}</Text>
+        </CardSection>
+      )
+    };
+  }
+
   render() {
     const { titleStyle } = styles;
+    const { id, title } = this.props.library
     return (
-      <CardSection>
-        <Text style={titleStyle}>
-          {this.props.library.title}
-        </Text>
-      </CardSection>
+      <TouchableWithoutFeedback
+          onPress={() => this.props.selectLibrary(id)}
+        >
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>
+              {title}
+            </Text>
+          </CardSection>
+          {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -22,4 +54,11 @@ const styles = {
   }
 };
 
-export default ListItem;
+// Own Props gets the props of the calling component
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+  return { expanded };
+};
+// first arg = mapStateToProps, second = actions object
+// Takes all of our actions and passes them to our item as props
+export default connect(mapStateToProps, actions)(ListItem);
